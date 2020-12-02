2b37bf376277dd02e4be332962168d050d50570c
SwaroopCK <swaroopkorde@gmail.com>
2020-12-02 11:10:40 +0530

    questionsadded

--- quiz.py
+++ quiz.py
@@ -1,5 +1,121 @@
 import tkinter
 from tkinter import *
+import random
+
+questions = [
+    "The series Friends is set in which city?",
+    "What pet did Ross own?",
+    "Monica briefly dates billionaire Pete Becker. Which country does he take her for their first date?",
+    "Rachel was popular in high school. Her prom date Chip ditched her for which girl at school?",
+    "Which cartoon character was on Phoebe’s thermos that Ursula threw under a bus?",
+    " What song is Phoebe best known for?",
+    "Which Friends character plays Dr. Drake Ramoray on the show Days Of Our Lives?",
+    " Who was Chandler’s TV magazine always addressed to?",
+    "Who sang the Friends theme?",
+    "What kind of uniform does Joey wear to Monica and Chandler’s wedding?",
+]
+
+answers_ch = [
+    ["1","2","3","4",],
+    ["1","2","3","4",],
+    ["1","2","3","4",],
+    ["1","2","3","4",],
+    ["Pebbles Flintstone","Yogi Bear","Judy Jetson","Bullwinkle",],
+    ["Smelly Cat","Smelly Dog","Smelly Rabbit","Smelly Worm",],
+    ["Ross Geller","Pete Becker","Eddie Menuek","Joey Tribbiani",],
+    ["Chanandler Bong","Chanandler Bang","Chanandler Bing","Chanandler Beng",],
+    ["The Banksys","The Rembrandts","The Constables","The Da Vinci Band",],
+    ["Chef","Soldier","Fire fighter","A baseball player",],
+]
+
+user_answer = []
+
+indexes = []
+def gen():
+    global indexes
+    while(len(indexes) < 5):
+        x = random.randint(0,9)
+        if x in indexes:
+            continue
+        else:
+            indexes.append(x)
+    #print(indexes)
+
+ques = 1
+def selected():
+    global radiovar,user_answer
+    global lblq,r1,r2,r3,r4
+    global ques
+    x = radiovar.get()
+    #print(x)
+    user_answer.append(x)
+    if ques < 5:
+        lblq.config(text = questions[indexes[ques]])
+        r1['text'] = answers_ch[indexes[ques]][0]
+        r2['text'] = answers_ch[indexes[ques]][1]
+        r3['text'] = answers_ch[indexes[ques]][2]
+        r4['text'] = answers_ch[indexes[ques]][3]
+        ques += 1 
+    else:
+        pass
+
+def startquiz():
+    global lblq,r1,r2,r3,r4
+    lblq = Label(
+        root,
+        text = questions[indexes[0]],
+        font = ("consolas",16),
+        width = 500,
+        justify = "center",
+        wraplength = 400,
+    )
+    lblq.pack()
+
+    global radiovar
+    radiovar = IntVar()
+    radiovar.set(-1)
+
+    r1 = Radiobutton(
+        root,
+        text = answers_ch[indexes[0]][0],
+        font = ("Times" , 12),
+        value = 0,
+        variable = radiovar,
+        command = selected,
+    )
+    r1.pack(pady = (40,0))
+
+    r2 = Radiobutton(
+        root,
+        text = answers_ch[indexes[0]][1],
+        font = ("Times" , 12),
+        value = 1,
+        variable = radiovar,
+        command = selected,
+    )
+    r2.pack(pady = (40,0))
+
+    r3 = Radiobutton(
+        root,
+        text = answers_ch[indexes[0]][2],
+        font = ("Times" , 12),
+        value = 2,
+        variable = radiovar,
+        command = selected,
+    )
+    r3.pack(pady = (40,0))
+
+    r4 = Radiobutton(
+        root,
+        text = answers_ch[indexes[0]][3],
+        font = ("Times" , 12),
+        value = 3,
+        variable = radiovar,
+        command = selected,
+    )
+    r4.pack(pady = (40,0))
+
+
 
 def startIsPressed():
     labelimage.destroy()
@@ -7,6 +123,8 @@ def startIsPressed():
     lblRules.destroy()
     lblInstruction.destroy()
     btnstart.destroy()
+    gen()
+    startquiz()
 
 root = tkinter.Tk()
 root.title("Quiz App")
